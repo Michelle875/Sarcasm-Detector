fe6d1ff79f34ec49064c8598c32f9344c4694beb
Michelle Shlivko <michelleshlivko@gmail.com>
2025-12-16 23:06:22 -0500

    completed stacking, merge issues

--- logitsticRegression.py
+++ logitsticRegression.py
@@ -118,3 +118,19 @@ test_preds = best_model.predict(X_test)
 test_acc = accuracy_score(y_test, test_preds)
 
 print("\nFINAL TEST ACCURACY:", test_acc)
+
+train_preds = best_model.predict(X_train)
+train_probs = best_model.predict_proba(X_train)
+train_pred_class_probs = train_probs[np.arange(len(train_preds)), train_preds]
+
+pd.DataFrame({"prediction": train_preds, "probability": train_pred_class_probs, "label": y_train}).to_csv("logisticRegression_train_preds.csv", index=False)
+
+valid_preds = best_model.predict(X_valid)
+valid_probs = best_model.predict_proba(X_valid)
+valid_pred_class_probs = valid_probs[np.arange(len(valid_preds)), valid_preds]
+pd.DataFrame({"prediction": valid_preds, "probability": valid_pred_class_probs, "label": y_valid}).to_csv("logisticRegression_valid_preds.csv", index=False)
+
+test_preds = best_model.predict(X_test)
+test_probs = best_model.predict_proba(X_test)
+test_pred_class_probs = test_probs[np.arange(len(test_preds)), test_preds]
+pd.DataFrame({"prediction": test_preds, "probability": test_pred_class_probs, "label": y_test}).to_csv("logisticRegression_test_preds.csv", index=False)
